b0cbf7c0927f6f622922b90505fdee636037f4e1
saroar <saroar9@gmail.com>
2017-09-25 17:49:08 +0300

    add test

--- lib/SimpleMail.rb
+++ lib/SimpleMail.rb
@@ -1,5 +1,235 @@
 require "SimpleMail/version"
+require 'mail'
+require 'base64'
+require 'socket'
 
 module SimpleMail
-  # Your code goes here...
+  @@options = {}
+  @@override_options = {}
+  @@subject_prefix = false
+  @@append_inputs = false
+
+# Default options can be set so that they don't have to be repeated.
+#
+#   SimpleMail.options = { :from => 'noreply@example.com', :via => :smtp, :via_options => { :host => 'smtp.yourserver.com' } }
+#   SimpleMail.mail(:to => 'foo@bar') # Sends mail to foo@bar from noreply@example.com using smtp
+#   SimpleMail.mail(:from => 'simple_mail@example.com', :to => 'foo@bar') # Sends mail to foo@bar from simple_mail@example.com using smtp
+  def self.options=(value)
+    @@options = value
+  end
+
+  def self.options()
+    @@options
+  end
+
+  def self.override_options=(value)
+    @@override_options = value
+  end
+
+  def self.override_options
+    @@override_options
+  end
+
+  def self.subject_prefix(value)
+    @@subject_prefix = value
+  end
+
+  def self.append_inputs
+    @@append_inputs = true
+  end
+
+# Send an email
+#   SimpleMail.mail(:to => 'you@example.com', :from => 'me@example.com', :subject => 'hi', :body => 'Hello there.')
+#   SimpleMail.mail(:to => 'you@example.com', :html_body => '<h1>Hello there!</h1>', :body => "In case you can't read html, Hello there.")
+#   SimpleMail.mail(:to => 'you@example.com', :cc => 'him@example.com', :from => 'me@example.com', :subject => 'hi', :body => 'Howsit!')
+  def self.mail(options)
+    if @@append_inputs
+      options[:body] = "#{options[:body]}/n #{options.to_s}"
+    end
+
+    options = @@options.merge options
+    options = options.merge @@override_options
+
+    if @@subject_prefix
+      options[:subject] = "#{@@subject_prefix}#{options[:subject]}"
+    end
+
+    fail ArgumentError, ':to is required' unless options[:to]
+
+    options[:via] = default_delivery_method unless options.key?(:via)
+
+    if options.key?(:via) && options[:via] == :sendmail
+      options[:via_options] ||= {}
+      options[:via_options][:location] ||= sendmail_binary
+    end
+
+    deliver build_mail(options)
+  end
+
+  def self.permissable_options
+    standard_options + non_standard_options
+  end
+
+  private
+
+  def self.deliver(mail)
+    mail.deliver!
+  end
+
+  def self.default_delivery_method
+    File.executable?(sendmail_binary) ? :sendmail : :smtp
+  end
+
+  def self.standard_options
+    [
+      :to,
+      :cc,
+      :bcc,
+      :from,
+      :subject,
+      :content_type,
+      :message_id,
+      :sender,
+      :reply_to,
+      :smtp_envelope_to
+    ]
+  end
+
+  def self.non_standard_options
+    [
+      :attachments,
+      :body,
+      :charset,
+      :enable_starttls_auto,
+      :headers,
+      :html_body,
+      :text_part_charset,
+      :via,
+      :via_options,
+      :body_part_header,
+      :html_body_part_header
+    ]
+  end
+
+  def self.build_mail(options)
+    mail = Mail.new do |m|
+      options[:date] ||= Time.now
+      options[:from] ||= 'simple_mail@unknown'
+      options[:via_options] ||= {}
+
+      options.each do |k, v|
+        next if SimpleMail.non_standard_options.include?(k)
+        m.send(k, v)
+      end
+
+      # Automatic handling of multipart messages in the underlying
+      # mail library works pretty well for the most part, but in
+      # the case where we have attachments AND text AND html bodies
+      # we need to explicitly define a second multipart/alternative
+      # boundary to encapsulate the body-parts within the
+      # multipart/mixed boundary that will be created automatically.
+      if options[:attachments] && options[:html_body] && options[:body]
+        part(:content_type => 'multipart/alternative') do |p|
+          p.html_part = SimpleMail.build_html_part(options)
+          p.text_part = SimpleMail.build_text_part(options)
+        end
+
+      # Otherwise if there is more than one part we still need to
+      # ensure that they are all declared to be separate.
+      elsif options[:html_body] || options[:attachments]
+        if options[:html_body]
+          m.html_part = SimpleMail.build_html_part(options)
+        end
+
+        if options[:body]
+          m.text_part = SimpleMail.build_text_part(options)
+        end
+
+      # If all we have is a text body, we don't need to worry about parts.
+      elsif options[:body]
+        body options[:body]
+      end
+
+      delivery_method options[:via], options[:via_options]
+    end
+
+    (options[:headers] ||= {}).each do |key, value|
+      mail[key] = value
+    end
+
+    add_attachments(mail, options[:attachments]) if options[:attachments]
+
+    mail.charset = options[:charset] if options[:charset] # charset must be set after setting content_type
+
+    if mail.multipart? && options[:text_part_charset]
+      mail.text_part.charset = options[:text_part_charset]
+    end
+    set_content_type(mail, options[:content_type])
+    mail
+  end
+
+  def self.build_html_part(options)
+    Mail::Part.new(:content_type => 'text/html;charset=UTF-8') do
+      content_transfer_encoding 'quoted-printable'
+      body Mail::Encodings::QuotedPrintable.encode(options[:html_body])
+      if options[:html_body_part_header] && options[:html_body_part_header].is_a?(Hash)
+        options[:html_body_part_header].each do |k,v|
+          header[k] = v
+        end
+      end
+    end
+  end
+
+  def self.build_text_part(options)
+    Mail::Part.new(:content_type => 'text/plain') do
+      content_type options[:charset] if options[:charset]
+      body options[:body]
+      if options[:body_part_header] && options[:body_part_header].is_a?(Hash)
+        options[:body_part_header].each do |k,v|
+          header[k] = v
+        end
+      end
+    end
+  end
+
+  def self.set_content_type(mail, user_content_type)
+    params = mail.content_type_parameters || {}
+    content_type =  case
+    when user_content_type
+       user_content_type
+    when mail.has_attachments?
+      if mail.attachments.detect { |a| a.inline? }
+        ["multipart", "related", params]
+      else
+        ["multipart", "mixed", params]
+      end
+    when mail.multipart?
+      ["multipart", "alternative", params]
+    else
+      false
+    end
+    mail.content_type = content_type if content_type
+  end
+
+  def self.add_attachments(mail, attachments)
+    attachments.each do |name, body|
+      name = name.gsub /\s+/, ' '
+
+      # mime-types wants to send these as "quoted-printable"
+      if name =~ /\.xlsx$/
+        mail.attachments[name] = {
+          :content => Base64.encode64(body),
+          :transfer_encoding => :base64
+        }
+      else
+        mail.attachments[name] = body
+      end
+      mail.attachments[name].add_content_id("<#{name}@#{Socket.gethostname}>")
+    end
+  end
+
+  def self.sendmail_binary
+    sendmail = `which sendmail`.chomp
+    sendmail.empty? ? '/usr/sbin/sendmail' : sendmail
+  end
 end
--- spec/SimpleMail_spec.rb
+++ spec/SimpleMail_spec.rb
@@ -5,7 +5,356 @@ RSpec.describe SimpleMail do
     expect(SimpleMail::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "has a version number" do
+    expect(SimpleMail::VERSION).not_to be nil
+  end
+
+  describe SimpleMail do
+
+    before(:each) do
+      allow(SimpleMail).to receive(:deliver)
+    end
+
+    it "sends mail" do
+      expect(SimpleMail).to receive(:deliver) do |mail|
+        expect(mail.to).to eq [ 'joe@example.com' ]
+        expect(mail.from).to eq [ 'sender@example.com' ]
+        expect(mail.subject).to eq 'hi'
+        expect(mail.body).to eq 'Hello, Joe.'
+      end
+      SimpleMail.mail(:to => 'joe@example.com', :from => 'sender@example.com', :subject => 'hi', :body => 'Hello, Joe.')
+    end
+
+    it "requires :to param" do
+      expect{ SimpleMail.mail({}) }.to raise_error(ArgumentError)
+    end
+
+    it "doesn't require any other param" do
+      expect{ SimpleMail.mail(:to => 'joe@example.com') }.to_not raise_error
+    end
+
+    it 'can list its available options' do
+      expect( SimpleMail.permissable_options ).to include(:to, :body)
+    end
+
+    describe "builds a Mail object with field:" do
+      it "to" do
+        expect(SimpleMail.build_mail(:to => 'joe@example.com').to).to eq [ 'joe@example.com' ]
+      end
+
+      it "to with multiple recipients" do
+        expect(SimpleMail.build_mail(:to => 'joe@example.com, friedrich@example.com').to).to eq [ 'joe@example.com', 'friedrich@example.com' ]
+      end
+
+      it "to with multiple recipients and names" do
+        expect(SimpleMail.build_mail(:to => 'joe@example.com, "Friedrich Hayek" <friedrich@example.com>').to).to eq [ 'joe@example.com', 'friedrich@example.com' ]
+      end
+
+      it "to with multiple recipients and names in an array" do
+        expect(SimpleMail.build_mail(:to => ['joe@example.com', '"Friedrich Hayek" <friedrich@example.com>']).to).to eq [ 'joe@example.com', 'friedrich@example.com' ]
+      end
+
+      it "cc" do
+        expect(SimpleMail.build_mail(:cc => 'joe@example.com').cc).to eq [ 'joe@example.com' ]
+      end
+
+      it "reply_to" do
+        expect(SimpleMail.build_mail(:reply_to => 'joe@example.com').reply_to).to eq [ 'joe@example.com' ]
+      end
+
+      it "cc with multiple recipients" do
+        expect(SimpleMail.build_mail(:cc => 'joe@example.com, friedrich@example.com').cc).to eq [ 'joe@example.com', 'friedrich@example.com' ]
+      end
+
+      it "from" do
+        expect(SimpleMail.build_mail(:from => 'joe@example.com').from).to eq [ 'joe@example.com' ]
+      end
+
+      it "bcc" do
+        expect(SimpleMail.build_mail(:bcc => 'joe@example.com').bcc).to eq [ 'joe@example.com' ]
+      end
+
+      it "bcc with multiple recipients" do
+        expect(SimpleMail.build_mail(:bcc => 'joe@example.com, friedrich@example.com').bcc).to eq [ 'joe@example.com', 'friedrich@example.com' ]
+      end
+
+      it "charset" do
+        mail = SimpleMail.build_mail(:charset => 'UTF-8')
+        expect(mail.charset).to eq 'UTF-8'
+      end
+
+      it "text_part_charset" do
+        mail = SimpleMail.build_mail(:attachments => {"foo.txt" => "content of foo.txt"}, :body => 'test', :text_part_charset => 'ISO-2022-JP')
+        expect(mail.text_part.charset).to eq 'ISO-2022-JP'
+      end
+
+      it "default charset" do
+        expect(SimpleMail.build_mail(body: 'body').charset).to eq 'UTF-8'
+        expect(SimpleMail.build_mail(html_body: 'body').charset).to eq 'UTF-8'
+      end
+
+      it "from (default)" do
+        expect(SimpleMail.build_mail({}).from).to eq [ 'simple_mail@unknown' ]
+      end
+
+      it "subject" do
+        expect(SimpleMail.build_mail(:subject => 'hello').subject).to eq 'hello'
+      end
+
+      it "body" do
+        expect(SimpleMail.build_mail(body: 'What do you know, Joe?').body)
+          .to eq 'What do you know, Joe?'
+      end
+
+      it "html_body" do
+        expect(SimpleMail.build_mail(html_body: 'What do you know, Joe?').parts.first.body)
+          .to eq 'What do you know, Joe?'
+        expect(SimpleMail.build_mail(html_body: 'What do you know, Joe?').parts.first.content_type)
+          .to eq 'text/html; charset=UTF-8'
+      end
+
+      it 'content_type' do
+        expect(SimpleMail.build_mail(content_type: 'multipart/related').content_type)
+          .to eq 'multipart/related'
+      end
+
+      it "date" do
+        now = Time.now
+        expect(SimpleMail.build_mail(:date => now).date).to eq DateTime.parse(now.to_s)
+      end
+
+      it "message_id" do
+        expect(SimpleMail.build_mail(:message_id => '<abc@def.com>').message_id).to eq 'abc@def.com'
+      end
+
+      it "custom headers" do
+        expect(SimpleMail.build_mail(:headers => {"List-ID" => "<abc@def.com>"})['List-ID'].to_s).to eq '<abc@def.com>'
+      end
+
+      it "sender" do
+        expect(SimpleMail.build_mail(:sender => "abc@def.com")['Sender'].to_s).to eq 'abc@def.com'
+      end
+
+      it "utf-8 encoded subject line" do
+        mail = SimpleMail.build_mail(:to => 'btp@foo', :subject => 'Café', :body => 'body body body')
+        expect(mail['subject'].encoded).to match( /^Subject: =\?UTF-8/ )
+      end
+
+      it "attachments" do
+        mail = SimpleMail.build_mail(:attachments => {"foo.txt" => "content of foo.txt"}, :body => 'test')
+        expect(mail.parts.length).to eq 2
+        expect(mail.parts.first.to_s).to match( /Content-Type: text\/plain/ )
+        expect(mail.attachments.first.content_id).to eq "<foo.txt@#{Socket.gethostname}>"
+      end
+
+      it "suggests mime-type" do
+        mail = SimpleMail.build_mail(:attachments => {"foo.pdf" => "content of foo.pdf"})
+        expect(mail.parts.length).to eq 1
+        expect(mail.parts.first.to_s).to match( /Content-Type: application\/pdf/ )
+        expect(mail.parts.first.to_s).to match( /filename=foo.pdf/ )
+        expect(mail.parts.first.content_transfer_encoding.to_s).to eq 'base64'
+      end
+
+      it "encodes xlsx files as base64" do
+        mail = SimpleMail.build_mail(:attachments => {"foo.xlsx" => "content of foo.xlsx"})
+        expect(mail.parts.length).to eq 1
+        expect(mail.parts.first.to_s).to match( /Content-Type: application\/vnd.openxmlformats-officedocument.spreadsheetml.sheet/ )
+        expect(mail.parts.first.to_s).to match( /filename=foo.xlsx/ )
+        expect(mail.parts.first.content_transfer_encoding.to_s).to eq 'base64'
+      end
+
+      it "passes cc and bcc as the list of recipients" do
+        mail = SimpleMail.build_mail(:to => ['to'], :cc => ['cc'], :from => ['from'], :bcc => ['bcc'])
+        expect(mail.destinations).to eq  ['to', 'cc', 'bcc']
+      end
+    end
+
+    describe "transport" do
+      it "transports via smtp if no sendmail binary" do
+        allow(SimpleMail).to receive(:sendmail_binary).and_return('/does/not/exist')
+        expect(SimpleMail).to receive(:build_mail).with(hash_including(:via => :smtp))
+        SimpleMail.mail(:to => 'foo@bar')
+      end
+
+      it "defaults to sendmail if no via is specified and sendmail exists" do
+        allow(File).to receive(:executable?).and_return(true)
+        expect(SimpleMail).to receive(:build_mail).with(hash_including(:via => :sendmail))
+        SimpleMail.mail(:to => 'foo@bar')
+      end
+
+      describe "SMTP transport" do
+
+        it "defaults to localhost as the SMTP server" do
+          mail = SimpleMail.build_mail(:to => "foo@bar", :enable_starttls_auto => true, :via => :smtp)
+          expect(mail.delivery_method.settings[:address]).to eq 'localhost'
+        end
+
+        it "enable starttls when tls option is true" do
+          mail = SimpleMail.build_mail(:to => "foo@bar", :enable_starttls_auto => true, :via => :smtp)
+          expect(mail.delivery_method.settings[:enable_starttls_auto]).to eq true
+        end
+      end
+    end
+
+    describe ":via option should over-ride the default transport mechanism" do
+      it "should send via sendmail if :via => sendmail" do
+        mail = SimpleMail.build_mail(:to => 'joe@example.com', :via => :sendmail)
+        expect(mail.delivery_method.kind_of?(Mail::Sendmail)).to eq true
+      end
+
+      it "should send via smtp if :via => smtp" do
+        mail = SimpleMail.build_mail(:to => 'joe@example.com', :via => :smtp)
+        expect(mail.delivery_method.kind_of?(Mail::SMTP)).to eq true
+      end
+
+    end
+
+    describe "sendmail binary location" do
+      it "should default to /usr/sbin/sendmail if not in path" do
+        allow(SimpleMail).to receive(:'`').and_return('')
+        expect(SimpleMail.sendmail_binary).to eq '/usr/sbin/sendmail'
+      end
+    end
+
+    describe "default options" do
+      it "should use default options" do
+        expect(SimpleMail).to receive(:build_mail).with(hash_including(:from => 'noreply@simple_mail'))
+        SimpleMail.options = { :from => 'noreply@simple_mail' }
+        SimpleMail.mail(:to => 'foo@bar')
+      end
+
+      it "should merge default options with options" do
+        expect(SimpleMail).to receive(:build_mail).with(hash_including(:from => 'override@simple_mail'))
+        SimpleMail.options = { :from => 'noreply@simple_mail' }
+        SimpleMail.mail(:from => 'override@simple_mail', :to => "foo@bar")
+      end
+
+      it "should return the default options" do
+        input = { :from => 'noreply@simple_mail' }
+        SimpleMail.options = input
+        output = SimpleMail.options
+        expect(output).to eq input
+      end
+    end
+
+    describe "override options" do
+      it "should use the overide options" do
+        expect(SimpleMail).to receive(:build_mail).with(hash_including(:from => 'reply@simple_mail'))
+
+        SimpleMail.override_options = { :from => 'reply@simple_mail' }
+        SimpleMail.mail(:to => 'foo@bar')
+      end
+
+      it "should use an override option instead of a default options" do
+        expect(SimpleMail).to receive(:build_mail).with(hash_including(:from => 'reply@simple_mail.com'))
+
+        SimpleMail.options = { :from => 'other_address@simple_mail.com' }
+        SimpleMail.override_options = { :from => 'reply@simple_mail.com' }
+        SimpleMail.mail(:to => 'foo@bar')
+      end
+
+      it "should use an override instead of a passed in value" do
+        expect(SimpleMail).to receive(:build_mail).with(hash_including(:from => 'reply@simple_mail.com'))
+
+        SimpleMail.override_options = { :from => 'reply@simple_mail.com' }
+        SimpleMail.mail(:to => 'foo@bar', :from => 'other_address@simple_mail.com')
+      end
+
+      it "should return the override options" do
+        input = { :from => 'reply@simple_mail' }
+        SimpleMail.override_options = input
+        output = SimpleMail.override_options
+
+        expect(output).to eq input
+      end
+    end
+
+    describe "subject prefix" do
+      after(:all) do
+        SimpleMail.subject_prefix(false)
+      end
+
+      it "should prefix email subject line with the given text" do
+        expect(SimpleMail).to receive(:build_mail).with(hash_including(:subject => 'First: Second'))
+
+        SimpleMail.subject_prefix('First: ')
+        SimpleMail.mail(:to => 'foo@bar', :subject => 'Second')
+      end
+
+      it "should set the prefix as the subject if no subject is given" do
+        expect(SimpleMail).to receive(:build_mail).with(hash_including(:subject => 'First: '))
+
+        SimpleMail.subject_prefix('First: ')
+        SimpleMail.mail(:to => 'foo@bar')
+      end
+    end
+
+    describe "append_inputs" do
+      it "appends the options passed into Pany.mail to the body" do
+        expect(SimpleMail).to receive(:build_mail).with(hash_including(:body => "body/n {:to=>\"foo@bar\", :body=>\"body\"}"))
+
+        SimpleMail.append_inputs
+
+        SimpleMail.mail(:to => 'foo@bar', :body => 'body')
+      end
+
+      it "sets the options passed into Pany.mail as the body if one is not present" do
+        expect(SimpleMail).to receive(:build_mail).with(hash_including(:body => "/n {:to=>\"foo@bar\"}"))
+
+        SimpleMail.append_inputs
+
+        SimpleMail.mail(:to => 'foo@bar')
+      end
+
+    end
+
+    describe "content type" do
+      context "mail with attachments, html_body and body " do
+        subject(:mail) do
+          SimpleMail.build_mail(
+            :body => 'test',
+            :html_body => 'What do you know, Joe?',
+            :attachments => {"foo.txt" => "content of foo.txt"},
+          )
+        end
+
+        it { expect(mail.parts.length).to eq 2 }
+        it { expect(mail.parts[0].parts.length).to eq 2 }
+        it { expect(mail.content_type.to_s).to include( 'multipart/mixed' ) }
+        it { expect(mail.parts[0].content_type.to_s).to include( 'multipart/alternative' ) }
+        it { expect(mail.parts[0].parts[0].to_s).to include( 'Content-Type: text/html' ) }
+        it { expect(mail.parts[0].parts[1].to_s).to include( 'Content-Type: text/plain' ) }
+        it { expect(mail.parts[1].to_s).to include( 'Content-Type: text/plain' ) }
+      end
+    end
+
+    describe "additional headers" do
+      subject(:mail) do
+        SimpleMail.build_mail(
+          :body => 'test',
+          :html_body => 'What do you know, Joe?',
+          :attachments => {"foo.txt" => "content of foo.txt"},
+          :body_part_header => { content_disposition: "inline"},
+          :html_body_part_header => { content_disposition: "inline"}
+        )
+      end
+
+      it { expect(mail.parts[0].parts[0].to_s).to include( 'inline' ) }
+      it { expect(mail.parts[0].parts[1].to_s).to include( 'inline' ) }
+
+      context "when parts aren't present" do
+        subject(:mail) do
+          SimpleMail.build_mail(
+            :body => 'test',
+            :body_part_header => { content_disposition: "inline"},
+            :html_body_part_header => { content_disposition: "inline"}
+          )
+        end
+
+        it { expect(mail.parts).to be_empty }
+        it { expect(mail.to_s).to_not include( 'inline' ) }
+      end
+    end
+
   end
 end
